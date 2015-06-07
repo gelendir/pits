b570e44e22689a7ff5da074a17475aa4ba476a3a
Gregory Eric Sanderson <gzou2000@gmail.com>
2015-06-07 15:46:39 -0400

    distribute letters according to weights

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import click
 import mido
 import random
-import string
+import bisect
 import tts
 import shutil
+import itertools
 
 KEYRANGE = range(21, 108)
 
@@ -36,6 +37,52 @@ class NoteAccumulator(object):
         return len(self.active) > 0
 
 
+@click.command()
+def devices():
+    for device in mido.get_input_names():
+        print(device)
+
+
+@click.command()
+@click.argument('device')
+@click.option('--filepath', '-f', default="audio.mp3", type=click.Path(dir_okay=False, writable=True))
+@click.option('--lettermap', '-l', default="letters.txt", type=click.Path(exists=True))
+def compose(device, filepath, lettermap):
+    letters = load_letters(lettermap)
+    keymap = generate_keymap(letters, KEYRANGE)
+
+    with mido.open_input(device) as port:
+        score = read_score(port)
+
+    text = generate_text(score, keymap)
+    generated_path = generate_tts(text)
+    shutil.move(generated_path, filepath)
+
+
+def load_letters(filepath):
+    with open(filepath, encoding="utf-8") as f:
+        lines = (l.split(" ") for l in f)
+        return {l[0]: int(l[1]) for l in lines}
+
+
+def generate_keymap(letters, keyrange):
+    keymap = {}
+    total = sum(letters.values())
+    weights = generate_weights(letters)
+    for note in keyrange:
+        rand = random.randint(0, total - 1)
+        choice = bisect.bisect(weights, (rand, None))
+        keymap[note] = weights[choice][1]
+    return keymap
+
+
+def generate_weights(letters):
+    pairs = letters.items()
+    letters = tuple(l[0] for l in pairs)
+    weights = itertools.accumulate(tuple(l[1] for l in pairs))
+    return tuple(zip(weights, letters))
+
+
 def read_score(port, end=108):
     score = []
     accumulator = NoteAccumulator()
@@ -54,15 +101,9 @@ def should_stop(event, end):
     return event.type == 'note_on' and event.note == end
 
 
-def generate_text(score):
-    keymap = generate_keymap()
+def generate_text(score, keymap):
     words = (map_word(notes, keymap) for notes in score)
     return " ".join(words)
-
-
-def generate_keymap():
-    return {note: random.choice(string.ascii_lowercase)
-            for note in KEYRANGE}
 
 
 def map_word(notes, keymap):
@@ -73,23 +114,6 @@ def map_word(notes, keymap):
 
 def generate_tts(text):
     return tts.generate(text)
-
-
-@click.command()
-def devices():
-    for device in mido.get_input_names():
-        print(device)
-
-
-@click.command()
-@click.argument('device')
-@click.argument('filepath', type=click.Path(dir_okay=False, writable=True))
-def compose(device, filepath):
-    with mido.open_input(device) as port:
-        score = read_score(port)
-    text = generate_text(score)
-    generated_path = generate_tts(text)
-    shutil.move(generated_path, filepath)
 
 
 if __name__ == "__main__":
