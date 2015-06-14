ce8450eae824a37cf33381e82f3ed56a79bfb25e
Gregory Eric Sanderson <gzou2000@gmail.com>
2015-06-14 17:30:31 -0400

    rename pits to pits_play

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'pits = pits.main:main',
+            'pits_play = pits.play:main',
         ]
     }
 )
